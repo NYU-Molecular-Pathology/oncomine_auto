ffed8ff5ee53d650450d661bc2503f16fc84d332
SummerYYY96 <summeryang96@gmail.com>
2025-09-15 19:39:33 -0400

    added sections that allow user to provide config path and workbook directory

--- myeloseq_report/myelo_watchdog.py
+++ myeloseq_report/myelo_watchdog.py
@@ -1,44 +1,55 @@
-__author__ = "Kelsey Zhu"
-__version__ = "1.0.3"
+__author__ = "Kelsey Zhu, Yiying Yang"
+__version__ = "1.1"
+
+import sys
 import time
 from watchdog.observers.polling import PollingObserver
 from watchdog.events import FileSystemEventHandler
 from myelo_worker import myeloseq
 
+
 class Watcher:
-    DIRECTORY_TO_WATCH = "/mnt/Z_drive/Molecular/IonTorrent/myeloseqer_test/worksheet.dropoffs/IR3"
-    def __init__(self):
+    def __init__(self, directory_to_watch, config_path):
+        self.DIRECTORY_TO_WATCH = directory_to_watch
+        self.config_path = config_path
         self.observer = PollingObserver()
 
     def run(self):
-        event_handler = Handler()
+        event_handler = Handler(self.config_path)
         self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
         self.observer.start()
         try:
             while True:
                 time.sleep(5)
-        except:
+        except KeyboardInterrupt:
             self.observer.stop()
-
         self.observer.join()
 
 
 class Handler(FileSystemEventHandler):
-    @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return None
+    def __init__(self, config_path):
+        self.config_path = config_path
 
-        elif event.event_type == 'created':
-            # Take any action here when a file is first created.
-            print("Received created event - %s." % event.src_path)
+    def on_any_event(self, event):
+        if event.is_directory:
+            return
+
+        if event.event_type == "created":
+            print(f"Received created event - {event.src_path}")
             time.sleep(5)
-            myelo_worker = myeloseq("/Users/yangy15/Documents/oncomine_auto_copy/ion_config.conf") 
+            myelo_worker = myeloseq(self.config_path)
             myelo_worker.workbook = event.src_path
             myelo_worker.start()
             del myelo_worker
 
-if __name__ == '__main__':
-    w = Watcher()
-    w.run()
 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: myelo_watchdog.py <config_path> <directory_to_watch>")
+        sys.exit(1)
+
+    config_path = sys.argv[1]
+    directory_to_watch = sys.argv[2]
+
+    w = Watcher(directory_to_watch, config_path)
+    w.run()
